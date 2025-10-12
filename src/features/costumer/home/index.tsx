754c5fdc4eff4fbd617e94794c718a58675c4079
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2025-10-12 06:22:39 -0300

    fix: home background size

--- src/features/costumer/home/index.tsx
+++ src/features/costumer/home/index.tsx
@@ -1,15 +1,19 @@
-import homeBackground from "../../../assets/images/hero-bg.svg";
+import background from "../../../assets/images/hero-bg.svg";
 import { Header } from "./components/Header";
 
 export function HomePage() {
   return (
     <section
       className="
-      min-h-screen w-full 
+      h-screen w-full 
       bg-no-repeat bg-top 
       text-white"
-      style={{ background: `url(${homeBackground})` }}
-    >
+      style={{
+        background: `url(${background})`,
+        backgroundPosition: "top center",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover"
+      }}>
       <Header />
     </section>
   );
